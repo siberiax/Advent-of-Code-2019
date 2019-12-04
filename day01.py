a72aa5bbbe4b86d3dbb7f33ee1a06e76198e4cf9
Alex Mchugh <alexmchugh@Alexs-MacBook-Pro.local>
2019-12-03 19:52:10 -0500

    Initial commit

---
+++ day01.py
@@ -0,0 +1,15 @@
+
+data = open('input.txt', 'r+').read().split()
+
+x = 0
+for line in data:
+    y = int(line)
+    while 1:
+        y //= 3
+        y -= 2
+        if y > 0:
+            x += y
+        else:
+            break
+
+print(x)
