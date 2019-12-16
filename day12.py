b0bb61781cb9f2e94f3ac42bcdbd6da886bbdf08
Alex Mchugh <alexmchugh@Alexs-MacBook-Pro.local>
2019-12-16 00:18:27 -0500

    Added part 1 back in

--- day12.py
+++ day12.py
@@ -14,6 +14,38 @@ for d in data:
     moons.append([int(i) for i in pos])
     vels.append([0,0,0])
 
+for _ in range(1000):
+    for i in range(len(moons)):
+        for j in range(len(moons)):
+            if i == j:
+                continue
+            m1 = moons[i]
+            m2 = moons[j]
+            for x in range(3):
+                 if m1[x] > m2[x]:
+                     vels[i][x] -= 1
+                 elif m1[x] < m2[x]:
+                     vels[i][x] += 1
+
+    for i in range(len(moons)):
+        for x in range(3):
+            moons[i][x] += vels[i][x]
+
+total_energy = 0
+
+for i in range(len(moons)):
+    m = moons[i]
+    v = vels[i]
+    pot = 0
+    kin = 0
+    for x in range(3):
+        pot += abs(m[x])
+        kin += abs(v[x])
+
+    total_energy += pot * kin
+
+print(total_energy)
+
 xyz = []
 for dim in range(3):
     seen = {}
