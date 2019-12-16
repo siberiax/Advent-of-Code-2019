06d4a15f843c5f726880034b7bef3acedcf54fd3
Alex Mchugh <alexmchugh@Alexs-MacBook-Pro.local>
2019-12-16 00:23:00 -0500

    put duplicate code in functions

--- day12.py
+++ day12.py
@@ -5,16 +5,7 @@ from math import gcd
 def lcm(a, b):
     return abs(a*b) // gcd(a, b)
 
-data = [line for line in open(sys.argv[1])]
-
-moons = []
-vels = []
-for d in data:
-    pos = re.findall(r'-?\d+',d)
-    moons.append([int(i) for i in pos])
-    vels.append([0,0,0])
-
-for _ in range(1000):
+def step():
     for i in range(len(moons)):
         for j in range(len(moons)):
             if i == j:
@@ -30,6 +21,25 @@ for _ in range(1000):
     for i in range(len(moons)):
         for x in range(3):
             moons[i][x] += vels[i][x]
+
+def makeState():
+    state = []
+    for i in range(len(moons)):
+        state.append(moons[i][dim])
+        state.append(vels[i][dim])
+    return str(state)
+
+data = [line for line in open(sys.argv[1])]
+
+moons = []
+vels = []
+for d in data:
+    pos = re.findall(r'-?\d+',d)
+    moons.append([int(i) for i in pos])
+    vels.append([0,0,0])
+
+for _ in range(1000):
+    step()
 
 total_energy = 0
 
@@ -51,38 +61,14 @@ for dim in range(3):
     seen = {}
     steps = 0
 
-    state = []
-    for i in range(len(moons)):
-        state.append(moons[i][dim])
-        state.append(vels[i][dim])
-
-    state = str(state)
+    state = makeState()
 
     while state not in seen:
         seen[state] = 1
         steps += 1
-        for i in range(len(moons)):
-            for j in range(len(moons)):
-                if i == j:
-                    continue
-                m1 = moons[i]
-                m2 = moons[j]
-                for x in range(3):
-                     if m1[x] > m2[x]:
-                         vels[i][x] -= 1
-                     elif m1[x] < m2[x]:
-                         vels[i][x] += 1
+        step()
 
-        for i in range(len(moons)):
-            for x in range(3):
-                moons[i][x] += vels[i][x]
-
-        state = []
-        for i in range(len(moons)):
-            state.append(moons[i][dim])
-            state.append(vels[i][dim])
-
-        state = str(state)
+        state = makeState()
 
     xyz.append(steps)
 
